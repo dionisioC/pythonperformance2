e9e0e763320af7e65330f8cd10a587f9ce253bda
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2019-09-26 23:29:02 +0200

    slots reviewed

---
+++ examples/08 - Slots/slots.py
@@ -0,0 +1,31 @@
+from memory_profiler import profile
+import config
+import time
+
+my_config = config.get_config()
+number_of_loops = my_config.getint("Global", "number_of_loops")
+
+
+class Slots:
+    __slots__ = ('x', 'y', 'z')
+
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+        self.x = self.y + self.z
+        self.y = self.x + self.z
+        self.z = self.y + self.x
+        self.x = self.x + self.z
+
+
+@profile
+def instanitate_classes():
+    objects = []
+    for i in range(number_of_loops):
+        x = Slots(i, i + 1, i + 2)
+        objects.append(x)
+
+
+start = time.time()
+instanitate_classes()
+end = time.time()
+print(end - start)
